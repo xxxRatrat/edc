a4423dbd6b09ca9deb980cc204d2f649bfbe5c18
xxxRatrat <98528749+xxxRatrat@users.noreply.github.com>
2023-05-11 17:21:19 +0800

    Update start-kafka-shell.sh

--- start-kafka-shell.sh
+++ start-kafka-shell.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -e HOST_IP=$1 -e ZK=$2 -i -t wurstmeister/kafka /bin/bash
+docker run --rm -p 9092:9092 -e KAFKA_ZOOKEEPER_CONNECT=zookeeper:2181 -v /var/run/docker.sock:/var/run/docker.sock -e HOST_IP=$1 -e ZK=$2 -i -t wurstmeister/kafka /bin/bash
